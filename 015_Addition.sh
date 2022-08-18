fbc8ea919841f1cc301f5f1f79543a4ee4430b05
Sandeep <sandeep12vish@gmail.com>
2022-08-18 23:59:31 +0530

    Addition

---
+++ 015_Addition.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Enter a number and hit \"Enter\""
+read user_number1;
+echo "Enter another number and hit \"Enter\""
+read user_number2;
+
+printf "You entered: %d and %d\n" $user_number1 $user_number2
+printf "Addition: %d\n" $(( user_number1 + user_number2))
