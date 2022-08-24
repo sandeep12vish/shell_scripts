16495e47515d2327dfb605bb82e77150c931690b
Sandeep <sandeep12vish@gmail.com>
2022-08-24 23:59:00 +0530

    function example

---
+++ 018_function.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Function example
+
+myFunction(){
+
+	echo 'Please enter any two number'
+	echo 'NUmber-1 :'
+	read number1;
+	echo 'Number-2:'
+	read number2;
+
+	echo 'Addition : $number1 + $number2' 
+
+}
+
+#function call
+myFunction
